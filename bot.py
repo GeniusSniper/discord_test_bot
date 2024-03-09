d30c3b9927235b04fc10bd1b0ee28b28f4659963
GeniusSniper <tiwei61342@gmail.com>
2024-03-09 15:21:56 -0500

    granting user role on joining server

--- bot.py
+++ bot.py
@@ -1,14 +1,21 @@
 # bot.py
 import os
+import random
 
 import discord
 from dotenv import load_dotenv
+from discord.utils import get
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
+ROLEID = int(os.getenv('DISCORD_ID'))
 
-client = discord.Client()
+# client = discord.Client(intents=discord.Intents.default())
+
+intents = discord.Intents.all()
+
+client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
@@ -20,5 +27,39 @@ async def on_ready():
         f'{client.user} is connected to the following guild:\n'
         f'{guild.name}(id: {guild.id})'
     )
+
+    members = '\n - '.join([member.name for member in guild.members])
+    print(f'Guild Members:\n - {members}')
+
+@client.event
+async def on_member_join(member):
+
+    role = get(member.guild.roles, id=ROLEID)
+    await member.add_roles(role)
+
+    await member.create_dm()
+    await member.dm_channel.send(
+        f'Hi {member.name}, welcome to my Discord server!'
+    )
+
+@client.event
+async def on_message(message):
+
+    if message.author == client.user:
+        return
+    
+    brooklyn_99_quotes = [
+        'I\'m the human form of the 💯 emoji.',
+        'Bingpot!',
+        (
+            'Cool. Cool cool cool cool cool cool cool, '
+            'no doubt no doubt no doubt no doubt.'
+        ),
+    ]
+    if message.content == '99!':
+        response = random.choice(brooklyn_99_quotes)
+        await message.channel.send(response)
+    elif message.content == 'raise-exception':
+        raise discord.DiscordException
 
 client.run(TOKEN)
