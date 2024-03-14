1ffd0903b31ed942f6f6c866191c405f8cb171d9
GeniusSniper <tiwei61342@gmail.com>
2024-03-14 13:53:19 -0400

    switching from using guild name to use guild id

--- bot.py
+++ bot.py
@@ -9,7 +9,8 @@ from discord.utils import get
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
-ROLEID = int(os.getenv('DISCORD_ID'))
+ROLEID = int(os.getenv('DISCORD_ROLE_ID'))
+GUILDID = int(os.getenv('DISCORD_GUILD_ID'))
 
 # client = discord.Client(intents=discord.Intents.default())
 
@@ -19,8 +20,9 @@ client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
+    print(client.guilds)
     for guild in client.guilds:
-        if guild.name == GUILD:
+        if guild.id == GUILDID:
             break
 
     print(
@@ -37,10 +39,10 @@ async def on_member_join(member):
     role = get(member.guild.roles, id=ROLEID)
     await member.add_roles(role)
 
-    await member.create_dm()
-    await member.dm_channel.send(
-        f'Hi {member.name}, welcome to my Discord server!'
-    )
+    # await member.create_dm()
+    # await member.dm_channel.send(
+    #     f'Hi {member.name}, welcome to my Discord server!'
+    # )
 
 @client.event
 async def on_message(message):
